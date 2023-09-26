6dac775210c360c6268d21ea72feb1665de1c46e
Arthur Bridges <killor@Arthurs-MBP.lan>
2023-09-25 23:53:26 -0500

    attempt to dynamically add repos to my Portfolio

--- asset/script.js
+++ asset/script.js
@@ -1,3 +1,19 @@
 /*use fetch with github api to fetch the
 repos thus, automatically updating the repos within
 the Portfolio*/
+var list = $("#ul-Projects");
+var projectRepo = $("#Project-repo");
+var requestUrl = 'https://api.github.com/users/Killorr/repos';
+
+function getApi () {
+    fetch(requestUrl).then(function (response){
+        return response.json();
+    }).then(function (data) {
+        for (var i = 0; i < data.length; i++) {
+            var listItem = $("<li>");
+            listItem.text(data[i].html_url);
+            list.append(listItem);
+        }
+    })
+}
+getApi();
