c5bc325b8dad31116202178b140c828b02d88e56
CodePro-ABridges <arthurbridges08@yahoo.com>
2024-02-13 20:54:40 -0600

    Fixing crashing

--- src/components/UI/ProjectsData.jsx
+++ src/components/UI/ProjectsData.jsx
@@ -1,10 +1,8 @@
-
 import TestTake from "../../images/testTake.png";
 import SocialAPI from "../../images/socialAPI.png";
 import TextEditor from "../../images/TextEditor.png";
 import Wip from "../../images/work-in-progress.jpeg";
-import RealTalk from '../../images/RealTalk.png';
- 
+import RealTalk from "../../images/RealTalk.png";
 
 export const Projects = [
   {
@@ -29,25 +27,27 @@ export const Projects = [
     github: "https://arthur-bridges.github.io/TestTaking-Website/",
   },
   {
-    id: 4, 
+    id: 4,
     name: "Book Bonaza",
     image: Wip,
-    techs: "Html, Javascript, React JS, Vite, Express JS, CSS, Node JS, MongoDB, & Mongoose",
+    techs:
+      "Html, Javascript, React JS, Vite, Express JS, CSS, Node JS, MongoDB, & Mongoose",
     github: "https://github.com/Arthur-Bridges/Book-Bonanza",
-  }, 
+  },
   {
     id: 5,
     name: "RealTalk",
     image: RealTalk,
-    techs: "MongoDB, Node JS, Javascript, Express JS, React JS, Socket.io, Tailwind CSS ",
+    techs:
+      "MongoDB, Node JS, Javascript, Express JS, React JS, Socket.io, Tailwind CSS ",
     github: "https://github.com/CodePro-ABridges/Real-Time-ChatApp",
   },
   {
     id: 6,
     name: "Quizard",
     image: Wip,
-    techs: "MongoDB, Node JS, Javascript, Express JS, React JS, Socket.io, Tailwind CSS ",
+    techs:
+      "MongoDB, Node JS, Javascript, Express JS, React JS, Socket.io, Tailwind CSS ",
     github: "https://github.com/wileland/Quizard",
   },
-
-  ;
+];
