d0749bebbcb8552db6103b065d2c70143d0dbc02
CodePro-ABridges <arthurbridges08@yahoo.com>
2024-02-13 20:41:45 -0600

    Updating Portfolio with new projects

--- src/components/UI/ProjectsData.jsx
+++ src/components/UI/ProjectsData.jsx
@@ -3,6 +3,7 @@ import TestTake from "../../images/testTake.png";
 import SocialAPI from "../../images/socialAPI.png";
 import TextEditor from "../../images/TextEditor.png";
 import Wip from "../../images/work-in-progress.jpeg";
+import RealTalk from '../../images/RealTalk.png';
  
 
 export const Projects = [
@@ -33,5 +34,20 @@ export const Projects = [
     image: Wip,
     techs: "Html, Javascript, React JS, Vite, Express JS, CSS, Node JS, MongoDB, & Mongoose",
     github: "https://github.com/Arthur-Bridges/Book-Bonanza",
+  }, 
+  {
+    id: 5,
+    name: "RealTalk",
+    image: RealTalk,
+    techs: "MongoDB, Node JS, Javascript, Express JS, React JS, Socket.io, Tailwind CSS ",
+    github: "https://github.com/CodePro-ABridges/Real-Time-ChatApp",
   },
+  {
+    id: 6,
+    name: "Quizard",
+    image: Wip,
+    techs: "MongoDB, Node JS, Javascript, Express JS, React JS, Socket.io, Tailwind CSS ",
+    github: "https://github.com/wileland/Quizard",
+  },
+
   ;
